fe02f16f54e0388872a66ae018261794d0afc411
Pierre Klink <dev@klinks.info>
2013-12-22 15:07:57 +0100

    added some tests

--- index.php
+++ index.php
@@ -4,40 +4,52 @@ require 'vendor/autoload.php';
 
 $app = new \Hahns\Hahns();
 
-$app->get('/', function () {
-    return 'hello home!';
+$app->get('/', function (\Hahns\Response\JsonImpl $response) {
+    return $response->send(['message' => 'home']);
 });
 
-$app->get('/hallo', function () {
-    return 'hello welt!';
+$app->get('/hallo', function (\Hahns\Response\JsonImpl $response) {
+    return $response->send(['message' => 'hallo welt!']);
 });
 
-$app->get('/hello', function () {
-    return 'hello world!';
+$app->get('/hello', function (\Hahns\Response\JsonImpl $response) {
+    return $response->send(['message' => 'hello world!']);
 });
 
 $app->get('/hello/[.*:name]', function (\Hahns\Request $request, \Hahns\Response\JsonImpl $response) {
-    return $response->send(['hello' => $request->get('name')]);
+    return $response->send([
+        'message' => sprintf('hello %s', $request->get('name'))
+    ]);
 });
 
 $app->get('/hallo/[.*:name]', function (\Hahns\Response\JsonImpl $response, \Hahns\Request $request) {
-    return $response->send(['hallo' => $request->get('name')]);
+    return $response->send([
+        'message' => sprintf('hallo %s', $request->get('name'))
+    ]);
 });
 
-$app->get('/hello/[.+:first]/[\d+:second]', function (\Hahns\Request $request) {
-    return sprintf('hello %s-%s', $request->get('first'), $request->get('second'));
+$app->get('/hello/[.+:first]/[\d+:second]', function (\Hahns\Request $request, \Hahns\Response\JsonImpl $response) {
+    return $response->send([
+        'message' => sprintf('hello %s-%d', $request->get('first'), $request->get('second'))
+    ]);
 });
 
-$app->get('/hallo/[.+:first]/[\d+:second]', function (\Hahns\Request $request) {
-    return sprintf('hallo %s-%s', $request->get('first'), $request->get('second'));
+$app->get('/hallo/[.+:first]/[\d+:second]', function (\Hahns\Request $request, \Hahns\Response\JsonImpl $response) {
+    return $response->send([
+        'message' => sprintf('hallo %s-%d', $request->get('first'), $request->get('second'))
+    ]);
 });
 
-$app->get('/hello/[.*:first]/[.*:second]', function (\Hahns\Request $request) {
-    return sprintf('hello %s and %s', $request->get('first'), $request->get('second'));
+$app->get('/hello/[.*:first]/[.*:second]', function (\Hahns\Request $request, \Hahns\Response\JsonImpl $response) {
+    return $response->send([
+        'message' => sprintf('hello %s %s', $request->get('first'), $request->get('second'))
+    ]);
 });
 
-$app->get('/hallo/[.*:first]/[.*:second]', function (\Hahns\Request $request) {
-    return sprintf('hallo %s und %s', $request->get('first'), $request->get('second'));
+$app->get('/hallo/[.*:first]/[.*:second]', function (\Hahns\Request $request, \Hahns\Response\JsonImpl $response) {
+    return $response->send([
+        'message' => sprintf('hallo %s %s', $request->get('first'), $request->get('second'))
+    ]);
 });
 
 
---
+++ tests/functional/JsonGetCept.php
@@ -0,0 +1,39 @@
+<?php
+$I = new TestGuy($scenario);
+$I->wantTo('perform actions and see result');
+
+$I->sendGET('/');
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson(['message' => 'home']);
+
+$I->sendGET('/hallo');
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson(['message' => 'hallo welt!']);
+
+$I->sendGET('/hello');
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson(['message' => 'hello world!']);
+
+$I->sendGET('/hallo/peter');
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson(['message' => 'hallo peter']);
+
+$I->sendGET('/hello/peter');
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson(['message' => 'hello peter']);
+
+$I->sendGET('/hello/peter/1337');
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson(['message' => 'hello peter-1337']);
+
+$I->sendGET('/hallo/peter/1337');
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson(['message' => 'hallo peter-1337']);
+
+$I->sendGET('/hello/peter/pan');
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson(['message' => 'hello peter pan']);
+
+$I->sendGET('/hallo/peter/pan');
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson(['message' => 'hallo peter pan']);
