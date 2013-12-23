8337ac9d4d5b774eac671ff263322185075b0d2e
Pierre Klink <dev@klinks.info>
2013-12-23 12:56:04 +0100

    added test for Request

---
+++ tests/unit/RequestTest.php
@@ -0,0 +1,48 @@
+<?php
+
+class RequestTest extends \Codeception\TestCase\Test
+{
+   /**
+    * @var \CodeGuy
+    */
+    protected $codeGuy;
+
+    /**
+     * @var \Hahns\Request
+     */
+    protected $instance;
+
+    protected function _before()
+    {
+        $this->instance = new \Hahns\Request();
+
+        // set get
+        $_GET = [
+            'string' => 'hello-get',
+            'array'  => ['get']
+        ];
+
+        // set post
+        $_POST = [
+            'string' => 'hello-post',
+            'array'  => ['post']
+        ];
+    }
+
+    public function testGet()
+    {
+        $this->assertEquals('hello-get', $this->instance->get('string'));
+        $this->assertEquals(['get'], $this->instance->get('array'));
+        $this->assertNull($this->instance->get('notexist'));
+        $this->assertTrue($this->instance->get('notexist', true));
+    }
+
+    public function testPost()
+    {
+        $this->assertEquals('hello-post', $this->instance->post('string'));
+        $this->assertEquals(['post'], $this->instance->post('array'));
+        $this->assertNull($this->instance->post('notexist'));
+        $this->assertTrue($this->instance->post('notexist', true));
+    }
+
+}
