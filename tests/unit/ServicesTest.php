4241f64cabfa9369b6a3efe45b269c1ee5d6bed9
Pierre Klink <dev@klinks.info>
2013-12-26 19:03:03 +0100

    improved ServiceTest

--- tests/unit/ServicesTest.php
+++ tests/unit/ServicesTest.php
@@ -3,6 +3,8 @@
 namespace Hahns\Test;
 
 use Codeception\TestCase\Test;
+use Hahns\Exception\ParameterMustBeAStringException;
+use Hahns\Exception\ServiceDoesNotExistException;
 use Hahns\Exception\ServiceMustBeAnObjectException;
 use Hahns\Services;
 
@@ -23,35 +25,31 @@ class ServicesTest extends Test
         $this->instance = new Services();
     }
 
-    public function testAllFine()
+    public function testGet()
     {
-        $this->instance->register('test', function () {
-            $service = new \stdClass();
-            $service->test = 'blah';
-            return $service;
-        });
+        $this->instance->register('test', function () { return new \stdClass(); });
+        $this->assertTrue($this->instance->get('test') instanceof \stdClass);
 
-        $this->assertEquals('blah', $this->instance->get('test')->test);
+        try {
+            $this->instance->get([]);
+            $this->fail();
+        } catch (ParameterMustBeAStringException $e) { }
+
+        try {
+            $this->instance->get('Asdasda');
+            $this->fail();
+        } catch (ServiceDoesNotExistException $e) { }
     }
 
-    /**
-     * @expectedException \ErrorException
-     */
-    public function testSetInvalidCallback()
+    public function testRegister()
     {
-        $this->instance->register('asdas', 1);
-    }
+        $this->instance->register('test', function () { return new \stdClass(); });
 
-    /**
-     * @expectedException \Hahns\Exception\ParameterMustBeAStringException
-     */
-    public function testInvalidName()
-    {
-        $this->instance->register([], function() {});
-    }
+        try {
+            $this->instance->register('asdas', 1);
+            $this->fail();
+        } catch (\ErrorException $e) { }
 
-    public function testCallbackReturnsNoObject()
-    {
         // return int
         $this->instance->register('test', function () {
             return 1;
@@ -59,29 +57,15 @@ class ServicesTest extends Test
 
         try {
             $this->instance->get('test');
-            $this->assertTrue(false);
-        } catch (ServiceMustBeAnObjectException $e) {
-            $this->assertTrue(true);
-        }
+            $this->fail();
+        } catch (ServiceMustBeAnObjectException $e) { }
 
         // return noting
-        $this->instance->register('test', function () {
-        });
+        $this->instance->register('test', function () {});
 
         try {
             $this->instance->get('test');
-            $this->assertTrue(false);
-        } catch (ServiceMustBeAnObjectException $e) {
-            $this->assertTrue(true);
-        }
+            $this->fail();
+        } catch (ServiceMustBeAnObjectException $e) { }
     }
-
-    /**
-     * @expectedException \Hahns\Exception\ServiceDoesNotExistException
-     */
-    public function testServiceDoesNotExist()
-    {
-        $this->instance->get('asdasdasd');
-    }
-
 }
